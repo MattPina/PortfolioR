20f0a40e89dfd196ab3656debf3af5de421e957c
SummonLight <51052465+SummonLight@users.noreply.github.com>
2020-08-19 14:06:34 +0000

    Add files via upload

---
+++ Codigos/Guardando nome em variavel e salvando em arquivo externo por comando/Exercicio1.R
@@ -0,0 +1,20 @@
+A<-1
+B<-2
+C<-3
+D<-4
+E<-5
+F<-6
+G<-7
+H<-8
+I<-9
+J<-10
+meunome<-'Nome'
+meurm<-123456
+
+#omedoArquivo<-readline(prompt = 'Informe o nome do arquivo: ')
+
+NomedoArquivo<-files.choose()
+
+save.image(file = NomedoArquivo)
+
+getwd()
